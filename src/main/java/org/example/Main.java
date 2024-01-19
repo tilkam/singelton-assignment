d519ffd5031bca38d1261202c4376785031a5bd5
Tilda Kampman <tilda.kampman@gmail.com>
2024-01-19 10:03:30 +0100

    add testcode for all four classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        RootUser user = new RootUser();
+        System.out.println(user);
+
+        //Lazy
         System.out.println(LazyRootUser.getInstance());
+        //Static final
         System.out.println(StaticRootUser.getInstance());
+        //Enum
         System.out.println(EnumRootUser.INSTANCE);
     }
 }
